e7e8a288f9221f598028de3e4ba0c47102f9f199
codydecoder <codycad1@yahoo.com>
2024-01-19 12:58:09 -0600

    Completed Color-Switcher

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Write your DOM code here!
+const red = document.querySelector('.red')
+red.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'red'
+    document.body.style.color = 'white'
+})
+
+const white = document.querySelector('.white')
+white.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = 'black'
+})
+
+const blue = document.querySelector('.blue')
+blue.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'blue'
+    document.body.style.color = 'white'
+})
+
+const yellow = document.querySelector('.yellow')
+yellow.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'yellow'
+    document.body.style.color = 'black'
+})
